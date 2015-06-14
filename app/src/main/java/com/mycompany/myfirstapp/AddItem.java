69bbb6b4aa96436e9d3cc8bcdd901437f89c1d24
amberylx <amberyl.x@gmail.com>
2015-06-14 13:06:33 -0700

    improvements to additem
    * changes to add item, made over a period of time, no longer sure what
    changes were made, please don’t ask

--- app/src/main/java/com/mycompany/myfirstapp/AddItem.java
+++ app/src/main/java/com/mycompany/myfirstapp/AddItem.java
@@ -1,51 +1,40 @@
 package com.mycompany.myfirstapp;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Build;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
 
-public class MyActivity extends Activity {
-    public final static String EXTRA_URLS = "com.mycompany.myfirstapp.URLS";
-    public final static Integer CONNECTION_TIMEOUT = 10000;
-    public final static Integer DATARETRIEVAL_TIMEOUT = 10000;
-    private static final String TAG = MyActivity.class.getSimpleName();
-
+public class AddItem extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_add_item);
+    }
+
+    public void addApiSubmit(View view) {
+        String title = "";
+        String text = "";
+        title = ((EditText)findViewById(R.id.item_title)).getText().toString();
+        text = ((EditText)findViewById(R.id.item_text)).getText().toString();
+        new CallAPI().execute("http://10.0.2.2:8000/api/item_add/", title, text);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         return true;
     }
@@ -61,29 +50,23 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void viewCollection(View view) {
-        new CallAPI().execute("http://10.0.2.2:8000/api/collection/");
-    }
-
-    public void addItem(View view) {
-        Intent intent = new Intent(getApplicationContext(), AddItem.class);
-        startActivity(intent);
-    }
-
-    private static void disableConnectionReuseIfNecessary() {
-        if (Integer.parseInt(Build.VERSION.SDK) < Build.VERSION_CODES.FROYO) {
-            System.setProperty("http.keepAlive", "false");
-        }
-    }
-
     private class CallAPI extends AsyncTask<String, String, JSONObject> {
+        HashMap<String, String> postParameters = new HashMap<String, String>();
+
         @Override
         protected JSONObject doInBackground(String... params) {
-            ApiCaller ac = new ApiCaller(params[0]);
-            return ac.makeGetRequest();
+            postParameters.put("title", params[1]);
+            postParameters.put("text", params[2]);
+            postParameters.put("collection", "1");
+            postParameters.put("times_collected", "1");
+            postParameters.put("active", "1");
+            ApiCaller ac = new ApiCaller(params[0], postParameters);
+            return ac.makePostRequest();
         }
 
         protected void onPostExecute(JSONObject result) {
+            System.out.println("******onpostexecute");
+            /*
             List<JSONObject> itemlist = new ArrayList<JSONObject>();
             List<String> urllist = new ArrayList<String>();
             try {
@@ -101,7 +84,7 @@ public class MyActivity extends Activity {
             }
 
             Intent intent = new Intent(getApplicationContext(), DisplayCollectionActivity.class);
-            startActivity(intent);
+            startActivity(intent);*/
         }
     }
 }
--- app/src/main/java/com/mycompany/myfirstapp/ApiCaller.java
+++ app/src/main/java/com/mycompany/myfirstapp/ApiCaller.java
@@ -6,22 +6,82 @@ import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ApiCaller {
     public String url;
+    public Map<String, String> parameters;
     public final static Integer CONNECTION_TIMEOUT = 10000;
     public final static Integer DATARETRIEVAL_TIMEOUT = 10000;
+    private static final char PARAMETER_DELIMITER = '&';
+    private static final char PARAMETER_EQUALS_CHAR = '=';
 
     public ApiCaller(String myUrl) {
         url = myUrl;
+        parameters = new HashMap<String, String>();
     }
 
-    public JSONObject callAPI() {
+    public ApiCaller(String myUrl, HashMap<String, String> myParameters) {
+        url = myUrl;
+        parameters = myParameters;
+    }
+
+    public JSONObject makePostRequest() {
+        HttpURLConnection urlConnection = null;
+        String postParameterString = createQueryStringForParameters(parameters);
+
+        try {
+            URL urlToRequest = new URL(url);
+            urlConnection = (HttpURLConnection)urlToRequest.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");
+            urlConnection.setConnectTimeout(CONNECTION_TIMEOUT);
+            urlConnection.setReadTimeout(DATARETRIEVAL_TIMEOUT);
+
+            urlConnection.setFixedLengthStreamingMode(
+                    postParameterString.getBytes().length);
+            PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+            out.print(postParameterString);
+            out.close();
+
+
+            int statusCode = urlConnection.getResponseCode();
+            System.out.println(statusCode);
+            if (statusCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
+            } else if (statusCode != HttpURLConnection.HTTP_OK) {
+            }
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            return new JSONObject(getResponseText(in));
+        } catch (MalformedURLException e) {
+            System.out.println(e);
+        } catch (SocketTimeoutException e) {
+            System.out.println(e);
+        } catch (JSONException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        return null;
+    }
+
+    public JSONObject makeGetRequest() {
         HttpURLConnection urlConnection = null;
 
         try {
@@ -53,6 +113,32 @@ public class ApiCaller {
         }
 
         return null;
+    }
+
+    public static String createQueryStringForParameters(Map<String, String> parameters) {
+        StringBuilder parametersAsQueryString = new StringBuilder();
+
+        try {
+            if (parameters != null) {
+                boolean firstParameter = true;
+
+                for (String parameterName : parameters.keySet()) {
+                    if (!firstParameter) {
+                        parametersAsQueryString.append(PARAMETER_DELIMITER);
+                    }
+
+                    parametersAsQueryString.append(parameterName)
+                            .append(PARAMETER_EQUALS_CHAR)
+                            .append(URLEncoder.encode(
+                                    parameters.get(parameterName), "UTF-8"));
+
+                    firstParameter = false;
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+        }
+
+        return parametersAsQueryString.toString();
     }
 
     private static String getResponseText(InputStream inStream) {
